ac81ee7cb2d5be0c135bb0014057ec2da634ac1d
delacroix <delacroix.jon@gmail.com>
2025-06-25 12:27:15 +0200

    activité 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css'
 import { Form, Button } from 'react-bootstrap'
 import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 function App() {
+  const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+
+  const schema = yup.object().shape({
+    name: yup.string().required("Le nom est requis").min(8, "Minimum 8 caractères").max(15, "Maximum 15 caractères"),
+    priority: yup.string().oneOf(["low", "medium", "high"]),
+    dueDate: yup.string().required("La date est requise").matches(dateRegex, "Format invalide. Ex: JJ/mm/YYYY").test("isValidDate", "La date ne doit pas antérieure à aujourd’hui", (value) => {
+        const [day, month, year] = value.split("/").map(Number);
+        const inputDate = new Date(year, month - 1, day);
+        return inputDate >= new Date;
+    }),
+    isCompleted: yup.boolean()
+  });
+
   const { register, handleSubmit, reset, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
     defaultValues: {
       name: "",
       dueDate: "",
-      priority: 'Basse',
+      priority: 'low',
       isCompleted: false,
     }
   });
@@ -21,7 +37,7 @@ function App() {
     <Form onSubmit={handleSubmit(onSubmit)}>
       <Form.Group className="mb-3" controlId="name">
         <Form.Label>Nom</Form.Label>
-        <Form.Control {...register("name", {required: "Le nom est requis"} )} isInvalid={!!errors.name} type="text" name="name" placeholder="Enter votre nom"/>
+        <Form.Control {...register("name")} isInvalid={!!errors.name} type="text" name="name" placeholder="Enter votre nom"/>
         <Form.Control.Feedback type="invalid">
             {errors.name?.message}
         </Form.Control.Feedback>
@@ -29,7 +45,7 @@ function App() {
 
       <Form.Group className="mb-3" controlId="dueDate">
         <Form.Label>Date</Form.Label>
-        <Form.Control {...register("dueDate", {required: "La date est requise"})} type="date" name="dueDate" isInvalid={!!errors.dueDate}/>
+        <Form.Control {...register("dueDate")} type="text" name="dueDate" isInvalid={!!errors.dueDate}/>
         <Form.Control.Feedback type="invalid">
             {errors.dueDate?.message}
         </Form.Control.Feedback>
@@ -38,9 +54,9 @@ function App() {
       <Form.Group className="mb-3" controlId="priority">
         <Form.Label>Priorité</Form.Label>
         <Form.Select {...register("priority")} type="select" name="priority">
-            <option value="Basse">Basse</option>
-            <option value="Moyenne">Moyenne</option>
-            <option value="Elevée">Elevée</option>
+            <option value="low">Basse</option>
+            <option value="medium">Moyenne</option>
+            <option value="high">Elevée</option>
         </Form.Select>
       </Form.Group>
 

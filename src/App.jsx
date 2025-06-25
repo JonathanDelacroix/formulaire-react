803b5f7b1c7eef410646f336a4d95d52905f012e
delacroix <delacroix.jon@gmail.com>
2025-06-25 10:05:03 +0200

    Activité 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
 import { useState } from 'react'
 import './App.css'
+import { Form, Button } from 'react-bootstrap'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [formData, setFormData] = useState({
+    name: "",
+    date: "",
+    priority: 'Basse',
+    isCompleted: false,
+  });
+
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+    ...prevData,
+    [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Formulaire soumis:', formData);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Form onSubmit={handleSubmit}>
+
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Nom</Form.Label>
+        <Form.Control type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Enter votre nom" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formDate">
+        <Form.Label>Date</Form.Label>
+        <Form.Control type="date" name="dueDate" value={formData.dueDate} onChange={handleChange} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formPriority">
+        <Form.Label>Priorité</Form.Label>
+        <Form.Select type="select" name="priority" value={formData.priority} onChange={handleChange}>
+            <option value="Basse">Basse</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Elevée">Elevée</option>
+        </Form.Select>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formIsCompleted">
+        <Form.Label>Terminé ?</Form.Label>
+        <Form.Check type="checkbox" name="isCompleted" checked={formData.isCompleted} onChange={handleChange} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Enregistrer
+      </Button>
+    </Form>
   )
 }
 
